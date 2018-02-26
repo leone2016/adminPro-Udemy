6be830ab570e34b35a541716b8c94513798b8d26
leoPatricio <leoz.31@hotmail.com>
2018-02-26 18:01:28 -0500

    hasta la clase 79

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable} from "rxjs/RX";
+import {observable} from "rxjs/symbol/observable";
+import {Subscription} from "rxjs/Rx";
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styles: []
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit,OnDestroy {
 
+  subscription:Subscription;
   constructor() {
     console.log("====================================");
     console.log(" WELCOME TO OBSERVABLE");
@@ -21,8 +24,8 @@ export class RxjsComponent implements OnInit {
 
 
     console.log(" existen dos tipo de obs.retry(), obs.retry(2), el primero ejecuta n veces, el otro solo 2 veces")
-    this.regresaObs().retry(2).subscribe(
-      numero=>console.log('subscrito',numero),
+    this.subscription = this.regresaObs().subscribe(
+      numero=>console.log('subscrito------- NUMERO IMPAR',numero),
       error => console.log('Error en el obs.',error),
       ()=>console.log('el observador termino')
 
@@ -33,23 +36,44 @@ export class RxjsComponent implements OnInit {
   ngOnInit() {
   }
 
-  regresaObs():Observable<number>{
+  regresaObs():Observable<any>{
+    console.log('clase 76 y 77 explica el map y Obserbable el map');
+   console.log(' MAP sirve para agregar un Obj que devuelva un observable');
     return  new Observable( observer =>{
       let contador=0;
       let interval= setInterval( ()=>{
         contador +=1;
-        observer.next( contador );
-        if( contador === 3){
-          clearInterval( interval );//para que el intervalo se detenga
-          observer.complete();
-        }
+        let salida = {valor:contador};
+        observer.next( salida );
+        // if( contador === 3){
+        //   clearInterval( interval );//para que el intervalo se detenga
+        //   observer.complete();
+        // }
 
-        if( contador ===2){
-          clearInterval( interval );//para que el intervalo se detenga
-          observer.error('AUXILIO jajaj verifica que funciona el error, tenia que llegar a 3 ');
+        // if( contador ===2){
+        //   clearInterval( interval );//para que el intervalo se detenga
+        //   observer.error('AUXILIO jajaj verifica que funciona el error, tenia que llegar a 3 ');
+        // }
+      },500);
+    }).retry(2).map( (res:any) =>{
+      return res.valor;
+    })
+      .filter( (valor,index) => {
+        console.log('filter',valor,index);
+        if ((valor % 2) === 1){
+          return true;
+        }else{
+          return false;
         }
-      },1000);
-    });
+         // filter siempre retorna un booleano
+        // filter permite controlar la informacion que es llamada desde el subcribe
+      });
+    // .retry(2) intenta dos veces, el MAP nunca se va a ejecutar si no se SUBSCRIBEN
   }
 
+  ngOnDestroy(){
+
+    console.log("la pagina se va cerrar");
+    this.subscription.unsubscribe();
+  }
 }
