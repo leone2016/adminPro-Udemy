0d6bde998bf3de58e46d98fb97739c8ffc86971b
leoPatricio <leoz.31@hotmail.com>
2018-03-05 17:44:50 -0500

    FIN SECCION 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivationEnd, Router} from "@angular/router";
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
+import {Title,Meta,MetaDefinition} from "@angular/platform-browser";
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
@@ -9,12 +10,23 @@ import 'rxjs/add/operator/map';
 })
 export class BreadcrumbsComponent implements OnInit {
 labelPage="";
-  constructor(private router: Router) {
+  constructor(private router: Router, public _title: Title, public _meta:Meta) {
     //filter retorna un true o un false;
-
+    //====================================================
+    // CLASE 80 explica como sacar las migas de pan
+    //====================================================
+    //importante revisar en angular.io/api/platform-broser/Title
+    //importante revisar en angular.io/api/platform-broser/Meta
       this.getDataRoute().subscribe( data =>{
         console.log(data);
         this.labelPage = data.titulo;
+        this._title.setTitle( this.labelPage);
+
+        let metaTag: MetaDefinition ={
+          name: 'description',
+          content:this.labelPage
+        }
+        this._meta.updateTag(metaTag);
       } );
   }
 
